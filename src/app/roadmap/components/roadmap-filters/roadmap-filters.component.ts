41a0c7849b71918a6d604e04e9fd1420e40f2795
slenane <slenane1@gmail.com>
2023-11-17 19:47:46 +0100

    Updated roadmap filters

--- src/app/roadmap/components/roadmap-filters/roadmap-filters.component.ts
+++ src/app/roadmap/components/roadmap-filters/roadmap-filters.component.ts
@@ -14,33 +14,18 @@ import {
   styleUrls: ["./roadmap-filters.component.scss"],
 })
 export class RoadmapFiltersComponent implements OnInit, OnChanges {
-  public showAllFilters: boolean = false;
   public sortedStack: any[] = [];
   public selectedView: "compact" | "expanded" = "compact";
   public selectedLanguage: any = null;
-  public selectedType: null | "frontend" | "backend" = null;
-  public typeConfig = [
-    { title: "Backend", name: "backend" },
-    { title: "Frontend", name: "frontend" },
-  ];
-  public statusConfig = [
-    { title: "In Progress", name: "progress" },
-    { title: "Completed", name: "completed" },
-  ];
   public timeConfig = [
-    { title: "1 Month", name: "month" },
     { title: "3 Months", name: "three" },
     { title: "6 Months", name: "six" },
     { title: "1 Year", name: "year" },
   ];
-  public selectedStatus: any;
   public selectedPeriod: any;
 
-  // @Input() typeConfig: any[];
   @Input() languageConfig: any[] = [];
-  @Output() onFilterType: EventEmitter<null | string> = new EventEmitter();
   @Output() onFilterLanguage: EventEmitter<null | string> = new EventEmitter();
-  @Output() onFilterStatus: EventEmitter<null | string> = new EventEmitter();
   @Output() onFilterPeriod: EventEmitter<null | string> = new EventEmitter();
 
   constructor() {}
@@ -63,18 +48,7 @@ export class RoadmapFiltersComponent implements OnInit, OnChanges {
     return stack.sort((a: any, b: any) => a.name.localeCompare(b.name));
   }
 
-  filterType($event: any) {
-    if (this.selectedType === $event) {
-      this.onFilterType.emit(null);
-      this.selectedType = null;
-    } else {
-      this.onFilterType.emit($event);
-      this.selectedType = $event;
-    }
-  }
-
   filterLanguage($event: any, language: string) {
-    console.log($event);
     $event.stopPropagation();
 
     if (this.selectedLanguage === language) {
@@ -96,13 +70,5 @@ export class RoadmapFiltersComponent implements OnInit, OnChanges {
     }
   }
 
-  filterStatus($event: any) {
-    if (this.selectedStatus === $event) {
-      this.onFilterStatus.emit(null);
-      this.selectedStatus = null;
-    } else {
-      this.onFilterStatus.emit($event);
-      this.selectedStatus = $event;
-    }
-  }
+  editRoadmap() {}
 }
