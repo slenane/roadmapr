c06cffbff6352bae8782228157f30ed63ea69cff
slenane <slenane1@gmail.com>
2023-11-27 16:31:51 +0100

    Updated roadmap projects

--- src/app/roadmap/components/roadmap-content/roadmap-projects/roadmap-projects.component.ts
+++ src/app/roadmap/components/roadmap-content/roadmap-projects/roadmap-projects.component.ts
@@ -1,10 +1,11 @@
 import {
   Component,
+  OnChanges,
   OnInit,
   Input,
   SimpleChanges,
-  OnChanges,
 } from "@angular/core";
+import { ProjectItem } from "src/app/projects/store/projects.models";
 
 @Component({
   selector: "app-roadmap-projects",
@@ -12,11 +13,33 @@ import {
   styleUrls: ["./roadmap-projects.component.scss"],
 })
 export class RoadmapProjectsComponent implements OnInit, OnChanges {
-  @Input() data: any;
+  public personal: number = 0;
+  public educational: number = 0;
+  public inProgress: number = 0;
+  public completed: number = 0;
+
+  @Input() data: ProjectItem[];
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngOnChanges(changes: SimpleChanges) {}
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes.data &&
+      changes.data.currentValue != changes.data.previousValue
+    ) {
+      this.generateData();
+    }
+  }
+
+  generateData() {
+    this.data.forEach((item: ProjectItem) => {
+      if (item.type === "personal") this.personal += 1;
+      else this.educational += 1;
+
+      if (item.endDate) this.completed += 1;
+      else this.inProgress += 1;
+    });
+  }
 }
