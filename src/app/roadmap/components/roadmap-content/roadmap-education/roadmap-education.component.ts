f9884da24a85206c8a3d14529b44f5dd91fcad9b
slenane <slenane1@gmail.com>
2023-12-03 15:04:40 +0100

    Updated education ssection and improved styles

--- src/app/roadmap/components/roadmap-content/roadmap-education/roadmap-education.component.ts
+++ src/app/roadmap/components/roadmap-content/roadmap-education/roadmap-education.component.ts
@@ -4,9 +4,30 @@ import {
   OnInit,
   Input,
   SimpleChanges,
+  ViewChild,
 } from "@angular/core";
 import { EducationItem } from "src/app/education/store/education.models";
 import { IStack } from "src/app/shared/constants/dev-paths.constants";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexYAxis,
+  ApexDataLabels,
+  ApexStroke,
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  theme: { mode: "dark" };
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  dataLabels: ApexDataLabels;
+  stroke: ApexStroke;
+};
 
 @Component({
   selector: "app-roadmap-education",
@@ -14,12 +35,17 @@ import { IStack } from "src/app/shared/constants/dev-paths.constants";
   styleUrls: ["./roadmap-education.component.scss"],
 })
 export class RoadmapEducationComponent implements OnInit, OnChanges {
-  public books: number = 0;
-  public courses: number = 0;
-  public inProgress: number = 0;
-  public completed: number = 0;
+  public books: number;
+  public courses: number;
+  public inProgress: number;
+  public completed: number;
+  public bookTrend: number;
+  public courseTrend: number;
   public completionRate: number;
   public stackCoverage: number;
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: ChartOptions;
 
   @Input() data: EducationItem[];
   @Input() userStack: IStack | undefined;
@@ -35,6 +61,7 @@ export class RoadmapEducationComponent implements OnInit, OnChanges {
       changes.data.currentValue != changes.data.previousValue
     ) {
       this.generateData();
+      this.generateChart();
     }
     if (
       (changes.userStack &&
@@ -47,6 +74,8 @@ export class RoadmapEducationComponent implements OnInit, OnChanges {
   }
 
   generateData() {
+    this.setInitialValues();
+
     this.data.forEach((item: EducationItem) => {
       if (item.type === "book") this.books += 1;
       else this.courses += 1;
@@ -73,5 +102,155 @@ export class RoadmapEducationComponent implements OnInit, OnChanges {
       count + completeStack.length !== 0
         ? Math.ceil((count / completeStack.length) * 100)
         : 0;
+  }
+
+  generateChart() {
+    const months = this.getPast12Months();
+    const [bookData, courseData] = this.getChartData(months);
+
+    this.chartOptions = {
+      theme: {
+        mode: "dark",
+      },
+      series: [
+        {
+          name: "Courses",
+          data: courseData,
+        },
+        {
+          name: "Books",
+          data: bookData,
+        },
+      ],
+      chart: {
+        height: 350,
+        type: "area",
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      xaxis: {
+        categories: months.map((month) => month.replace(/\s\d{4}/, "")),
+      },
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return (+val).toFixed(0).toString();
+          },
+        },
+      },
+    };
+  }
+
+  getPast12Months() {
+    const today = new Date();
+    const past12Months = [];
+
+    for (let i = 0; i < 12; i++) {
+      const currentMonth = today.getMonth() - i;
+      const adjustedMonth = (currentMonth + 12) % 12;
+      const adjustedYear =
+        today.getFullYear() - Math.floor((today.getMonth() - i) / 12);
+
+      const formattedDate = new Date(
+        adjustedYear,
+        adjustedMonth,
+        1
+      ).toLocaleString("en-us", { month: "short", year: "numeric" });
+
+      past12Months.unshift(formattedDate);
+    }
+
+    return past12Months;
+  }
+
+  getChartData(months: string[]): [number[], number[]] {
+    const bookObject: any = {},
+      courseObject: any = {};
+    let booksLastPeriod = 0,
+      coursesLastPeriod = 0;
+
+    const books = this.data.filter((item: EducationItem) => {
+      return item.type === "book" && item.status === "done";
+    });
+    const courses = this.data.filter((item: EducationItem) => {
+      return item.type === "course" && item.status === "done";
+    });
+
+    books.forEach((book) => {
+      const period = new Date(book.endDate).toLocaleString("en-us", {
+        month: "short",
+        year: "numeric",
+      });
+
+      if (months.indexOf(period) !== -1) {
+        if (bookObject[period]) bookObject[period]++;
+        else bookObject[period] = 1;
+      } else {
+        booksLastPeriod++;
+      }
+    });
+
+    courses.forEach((course) => {
+      const period = new Date(course.endDate).toLocaleString("en-us", {
+        month: "short",
+        year: "numeric",
+      });
+
+      if (months.indexOf(period) !== -1) {
+        if (courseObject[period]) courseObject[period]++;
+        else courseObject[period] = 1;
+      } else {
+        coursesLastPeriod++;
+      }
+    });
+
+    const bookData = this.getAccumulatedData(
+      months,
+      booksLastPeriod,
+      bookObject
+    );
+    const courseData = this.getAccumulatedData(
+      months,
+      coursesLastPeriod,
+      courseObject
+    );
+
+    this.bookTrend =
+      bookData[bookData.length - 1] - bookData[bookData.length - 2];
+    this.courseTrend =
+      courseData[courseData.length - 1] - courseData[courseData.length - 2];
+
+    return [bookData, courseData];
+  }
+
+  getAccumulatedData(array: string[], initial: number, obj: any) {
+    let result = initial;
+    const accumulatedValues = [];
+
+    for (const key of array) {
+      if (obj.hasOwnProperty(key)) {
+        result += obj[key];
+      }
+      accumulatedValues.push(result);
+    }
+
+    return accumulatedValues;
+  }
+
+  setInitialValues() {
+    this.books = 0;
+    this.courses = 0;
+    this.inProgress = 0;
+    this.completed = 0;
   }
 }
--- src/app/roadmap/components/roadmap-content/roadmap-projects/roadmap-projects.component.ts
+++ src/app/roadmap/components/roadmap-content/roadmap-projects/roadmap-projects.component.ts
@@ -15,6 +15,8 @@ import {
 } from "ng-apexcharts";
 
 type ChartOptions = {
+  fill: { colors: string[] };
+  theme: { mode: "dark" };
   series: ApexAxisChartSeries;
   chart: ApexChart;
   xaxis: ApexXAxis;
@@ -28,6 +30,8 @@ type ChartOptions = {
 export class RoadmapProjectsComponent implements OnInit, OnChanges {
   public personal: number = 0;
   public educational: number = 0;
+  public personalTrend: number = 0;
+  public educationalTrend: number = 0;
   public inProgress: number = 0;
   public completed: number = 0;
 
@@ -56,8 +60,13 @@ export class RoadmapProjectsComponent implements OnInit, OnChanges {
       if (item.type === "personal") this.personal += 1;
       else this.educational += 1;
 
-      if (item.endDate) this.completed += 1;
-      else this.inProgress += 1;
+      if (item.endDate) {
+        this.completed += 1;
+        if (this.isInCurrentMonth(item.endDate)) {
+          if (item.type === "personal") this.personalTrend += 1;
+          else this.educationalTrend += 1;
+        }
+      } else this.inProgress += 1;
     });
   }
 
@@ -83,6 +92,12 @@ export class RoadmapProjectsComponent implements OnInit, OnChanges {
 
     if (list.length) {
       this.chartOptions = {
+        fill: {
+          colors: ["#00bcd4"],
+        },
+        theme: {
+          mode: "dark",
+        },
         series: [
           {
             name: "Languages used",
@@ -101,5 +116,14 @@ export class RoadmapProjectsComponent implements OnInit, OnChanges {
         },
       };
     }
+  }
+
+  isInCurrentMonth(date: Date) {
+    const today = new Date();
+    const current = new Date(date);
+    return (
+      current.getMonth() === today.getMonth() &&
+      current.getFullYear() === today.getFullYear()
+    );
   }
 }
