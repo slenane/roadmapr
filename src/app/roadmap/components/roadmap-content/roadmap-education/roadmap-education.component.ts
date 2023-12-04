e37925d337bb08f262b2ad7da8c8022bec779573
slenane <slenane1@gmail.com>
2023-12-04 17:13:14 +0100

    Updated education styles

--- src/app/roadmap/components/roadmap-content/roadmap-education/roadmap-education.component.ts
+++ src/app/roadmap/components/roadmap-content/roadmap-education/roadmap-education.component.ts
@@ -123,7 +123,7 @@ export class RoadmapEducationComponent implements OnInit, OnChanges {
         },
       ],
       chart: {
-        height: 350,
+        height: 250,
         type: "area",
         zoom: {
           enabled: false,
