955d479a9e30d76db0c47177f8cdff9c297bfd00
slenane <slenane1@gmail.com>
2024-01-15 19:35:23 +0100

    Updated landing styles, language, GA and images

--- src/app/auth/components/landing/landing.component.ts
+++ src/app/auth/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-landing",
@@ -6,7 +6,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
+  public scrolling = false;
+  public currentYear: number;
+
+  @ViewChild("landing") landing: ElementRef;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
+
+  ngAfterViewInit() {
+    if (this.landing) {
+      this.landing.nativeElement.addEventListener(
+        "scroll",
+        this.onScroll.bind(this)
+      );
+    }
+  }
+
+  onScroll(event: Event): void {
+    const target = event.target as HTMLElement;
+    this.scrolling = target.scrollTop > 64;
+  }
 }
